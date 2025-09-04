1759f13687c3b769387c212af4d6956e6d071fcf
yogeshwaran-93 <yogeshwaransyogeshwarans4@gmail.com>
2025-09-04 14:34:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function openModal(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+// Close modal on outside click
+window.onclick = function(event) {
+  const modals = document.querySelectorAll('.modal');
+  modals.forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+};
